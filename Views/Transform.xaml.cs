e034332c0b325a332c57a6286e92d9bbcacb3c5b
PKuczynski <przemek.ku2000@gmail.com>
2023-10-26 23:40:54 +0200

    .

--- Views/Transform.xaml.cs
+++ Views/Transform.xaml.cs
@@ -90,7 +90,7 @@ namespace Grafika.Views
         {
             if (double.TryParse(ValueTextBox.Text, out double value))
             {
-                ApplyArithmeticOperation(value,true);
+                ApplyArithmeticOperation(value, true);
             }
             else
             {
@@ -102,7 +102,7 @@ namespace Grafika.Views
         {
             if (double.TryParse(ValueTextBox.Text, out double value) && value != 0)
             {
-                ApplyArithmeticOperation(1.0 / value,true);
+                ApplyArithmeticOperation(1.0 / value, true);
             }
             else
             {
@@ -125,8 +125,8 @@ namespace Grafika.Views
         private void ApplyGrayscale_Click(object sender, RoutedEventArgs e)
         {
             ApplyToGrayscale();
-        }  
-        
+        }
+
         private void ApplyGrayscale2_Click(object sender, RoutedEventArgs e)
         {
             ApplyToGrayscaleAlternative();
@@ -275,7 +275,7 @@ namespace Grafika.Views
 
                         pixelData[offset + 2] = grayValue;
                         pixelData[offset + 1] = grayValue;
-                        pixelData[offset] = grayValue; 
+                        pixelData[offset] = grayValue;
                     }
                 }
 
@@ -321,10 +321,330 @@ namespace Grafika.Views
 
         private void TransformRadio_Checked(object sender, RoutedEventArgs e)
         {
-
+            if (transformGrid != null && filtersGrid != null && valueFieldGrid != null)
+            {
+                transformGrid.Visibility = Visibility.Visible;
+                filtersGrid.Visibility = Visibility.Collapsed;
+                valueFieldGrid.Visibility = Visibility.Visible;
+            }
         }
 
         private void FiltersRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            if (transformGrid != null && filtersGrid != null && valueFieldGrid != null)
+            {
+                transformGrid.Visibility = Visibility.Collapsed;
+                filtersGrid.Visibility = Visibility.Visible;
+                valueFieldGrid.Visibility = Visibility.Collapsed;
+            }
+        }
+
+
+        private void ApplyAverageFilter_Click(object sender, RoutedEventArgs e)
+        {
+            if (originalImage != null)
+            {
+                int width = originalImage.PixelWidth;
+                int height = originalImage.PixelHeight;
+                int stride = width * 4;
+                byte[] pixelData = new byte[height * stride];
+                originalImage.CopyPixels(pixelData, stride, 0);
+
+                int kernelSize = 3; // Rozmiar maski (np. 3x3)
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        int offset = y * stride + x * 4;
+
+                        int totalR = 0, totalG = 0, totalB = 0;
+                        int pixelCount = 0;
+
+                        for (int ky = -kernelSize / 2; ky <= kernelSize / 2; ky++)
+                        {
+                            for (int kx = -kernelSize / 2; kx <= kernelSize / 2; kx++)
+                            {
+                                int nx = x + kx;
+                                int ny = y + ky;
+
+                                if (nx >= 0 && nx < width && ny >= 0 && ny < height)
+                                {
+                                    int kernelOffset = ny * stride + nx * 4;
+                                    totalR += pixelData[kernelOffset + 2];
+                                    totalG += pixelData[kernelOffset + 1];
+                                    totalB += pixelData[kernelOffset];
+                                    pixelCount++;
+                                }
+                            }
+                        }
+
+                        byte newR = (byte)(totalR / pixelCount);
+                        byte newG = (byte)(totalG / pixelCount);
+                        byte newB = (byte)(totalB / pixelCount);
+
+                        pixelData[offset + 2] = newR;
+                        pixelData[offset + 1] = newG;
+                        pixelData[offset] = newB;
+                    }
+                }
+
+                BitmapSource processedBitmap = BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgr32, null, pixelData, stride);
+                originalImage = ConvertBitmapSourceToBitmapImage(processedBitmap);
+                displayedImage.Source = processedBitmap;
+            }
+            else
+            {
+                MessageBox.Show("Najpierw wczytaj obraz.");
+            }
+        }
+
+        private void ApplyMedianFilter_Click(object sender, RoutedEventArgs e)
+        {
+            if (originalImage != null)
+            {
+                int width = originalImage.PixelWidth;
+                int height = originalImage.PixelHeight;
+
+                int stride = width * 4;
+                byte[] pixelData = new byte[height * stride];
+                originalImage.CopyPixels(pixelData, stride, 0);
+
+                for (int y = 1; y < height - 1; y++)
+                {
+                    for (int x = 1; x < width - 1; x++)
+                    {
+                        int offset = y * stride + x * 4;
+
+                        byte[] redValues = new byte[9];
+                        byte[] greenValues = new byte[9];
+                        byte[] blueValues = new byte[9];
+
+                        int index = 0;
+                        for (int i = -1; i <= 1; i++)
+                        {
+                            for (int j = -1; j <= 1; j++)
+                            {
+                                int neighborOffset = (y + i) * stride + (x + j) * 4;
+                                redValues[index] = pixelData[neighborOffset + 2];
+                                greenValues[index] = pixelData[neighborOffset + 1];
+                                blueValues[index] = pixelData[neighborOffset];
+                                index++;
+                            }
+                        }
+
+                        Array.Sort(redValues);
+                        Array.Sort(greenValues);
+                        Array.Sort(blueValues);
+
+                        byte newR = redValues[4];
+                        byte newG = greenValues[4];
+                        byte newB = blueValues[4];
+
+                        pixelData[offset + 2] = newR;
+                        pixelData[offset + 1] = newG;
+                        pixelData[offset] = newB;
+                    }
+                }
+
+                BitmapSource processedBitmap = BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgr32, null, pixelData, stride);
+                originalImage = ConvertBitmapSourceToBitmapImage(processedBitmap);
+                displayedImage.Source = processedBitmap;
+            }
+            else
+            {
+                MessageBox.Show("Najpierw wczytaj obraz.");
+            }
+        }
+
+        private void ApplySobelFilter_Click(object sender, RoutedEventArgs e)
+        {
+            if (originalImage != null)
+            {
+                int width = originalImage.PixelWidth;
+                int height = originalImage.PixelHeight;
+
+                int stride = width * 4;
+                byte[] pixelData = new byte[height * stride];
+                originalImage.CopyPixels(pixelData, stride, 0);
+
+                byte[] newPixelData = new byte[pixelData.Length];
+                Array.Copy(pixelData, newPixelData, pixelData.Length);
+
+                int[] sobelX = { -1, 0, 1, -2, 0, 2, -1, 0, 1 };
+                int[] sobelY = { -1, -2, -1, 0, 0, 0, 1, 2, 1 };
+
+                for (int y = 1; y < height - 1; y++)
+                {
+                    for (int x = 1; x < width - 1; x++)
+                    {
+                        int offset = y * stride + x * 4;
+
+                        int redX = 0, greenX = 0, blueX = 0;
+                        int redY = 0, greenY = 0, blueY = 0;
+
+                        for (int i = -1; i <= 1; i++)
+                        {
+                            for (int j = -1; j <= 1; j++)
+                            {
+                                int neighborOffset = (y + i) * stride + (x + j) * 4;
+                                int sobelIndex = (i + 1) * 3 + (j + 1);
+
+                                redX += pixelData[neighborOffset + 2] * sobelX[sobelIndex];
+                                greenX += pixelData[neighborOffset + 1] * sobelX[sobelIndex];
+                                blueX += pixelData[neighborOffset] * sobelX[sobelIndex];
+
+                                redY += pixelData[neighborOffset + 2] * sobelY[sobelIndex];
+                                greenY += pixelData[neighborOffset + 1] * sobelY[sobelIndex];
+                                blueY += pixelData[neighborOffset] * sobelY[sobelIndex];
+                            }
+                        }
+
+                        int newRed = (int)Math.Sqrt(redX * redX + redY * redY);
+                        int newGreen = (int)Math.Sqrt(greenX * greenX + greenY * greenY);
+                        int newBlue = (int)Math.Sqrt(blueX * blueX + blueY * blueY);
+
+                        newPixelData[offset + 2] = (byte)Math.Min(255, newRed);
+                        newPixelData[offset + 1] = (byte)Math.Min(255, newGreen);
+                        newPixelData[offset] = (byte)Math.Min(255, newBlue);
+                    }
+                }
+
+                BitmapSource processedBitmap = BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgr32, null, newPixelData, stride);
+                originalImage = ConvertBitmapSourceToBitmapImage(processedBitmap);
+                displayedImage.Source = processedBitmap;
+            }
+            else
+            {
+                MessageBox.Show("Najpierw wczytaj obraz.");
+            }
+        }
+
+        private void ApplyHighPassFilter_Click(object sender, RoutedEventArgs e)
+        {
+            if (originalImage != null)
+            {
+                int width = originalImage.PixelWidth;
+                int height = originalImage.PixelHeight;
+                int stride = width * 4;
+                byte[] pixelData = new byte[height * stride];
+                originalImage.CopyPixels(pixelData, stride, 0);
+
+                int kernelSize = 3;
+                int[,] kernel = {
+                    { -1, -1, -1 },
+                    { -1,  9, -1 },
+                    { -1, -1, -1 }
+                };
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        int offset = y * stride + x * 4;
+
+                        int totalR = 0, totalG = 0, totalB = 0;
+
+                        for (int ky = -1; ky <= 1; ky++)
+                        {
+                            for (int kx = -1; kx <= 1; kx++)
+                            {
+                                int nx = x + kx;
+                                int ny = y + ky;
+
+                                if (nx >= 0 && nx < width && ny >= 0 && ny < height)
+                                {
+                                    int kernelOffset = ny * stride + nx * 4;
+                                    totalR += pixelData[kernelOffset + 2] * kernel[ky + 1, kx + 1];
+                                    totalG += pixelData[kernelOffset + 1] * kernel[ky + 1, kx + 1];
+                                    totalB += pixelData[kernelOffset] * kernel[ky + 1, kx + 1];
+                                }
+                            }
+                        }
+
+                        byte newR = (byte)Math.Max(0, Math.Min(255, totalR));
+                        byte newG = (byte)Math.Max(0, Math.Min(255, totalG));
+                        byte newB = (byte)Math.Max(0, Math.Min(255, totalB));
+
+                        pixelData[offset + 2] = newR;
+                        pixelData[offset + 1] = newG;
+                        pixelData[offset] = newB;
+                    }
+                }
+
+                BitmapSource processedBitmap = BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgr32, null, pixelData, stride);
+                originalImage = ConvertBitmapSourceToBitmapImage(processedBitmap);
+                displayedImage.Source = processedBitmap;
+            }
+            else
+            {
+                MessageBox.Show("Najpierw wczytaj obraz.");
+            }
+        }
+
+        private void ApplyGaussianBlurFilter_Click(object sender, RoutedEventArgs e)
+        {
+            if (originalImage != null)
+            {
+                int width = originalImage.PixelWidth;
+                int height = originalImage.PixelHeight;
+
+                int stride = width * 4;
+                byte[] pixelData = new byte[height * stride];
+                originalImage.CopyPixels(pixelData, stride, 0);
+
+                byte[] newPixelData = new byte[pixelData.Length];
+                Array.Copy(pixelData, newPixelData, pixelData.Length);
+
+                double[] kernel = {
+            1, 2, 1,
+            2, 4, 2,
+            1, 2, 1
+        };
+
+                double kernelSum = kernel.Sum();
+
+                for (int y = 1; y < height - 1; y++)
+                {
+                    for (int x = 1; x < width - 1; x++)
+                    {
+                        int offset = y * stride + x * 4;
+
+                        double red = 0, green = 0, blue = 0;
+
+                        for (int i = -1; i <= 1; i++)
+                        {
+                            for (int j = -1; j <= 1; j++)
+                            {
+                                int neighborOffset = (y + i) * stride + (x + j) * 4;
+
+                                red += pixelData[neighborOffset + 2] * kernel[(i + 1) * 3 + (j + 1)];
+                                green += pixelData[neighborOffset + 1] * kernel[(i + 1) * 3 + (j + 1)];
+                                blue += pixelData[neighborOffset] * kernel[(i + 1) * 3 + (j + 1)];
+                            }
+                        }
+
+                        red /= kernelSum;
+                        green /= kernelSum;
+                        blue /= kernelSum;
+
+                        newPixelData[offset + 2] = (byte)Math.Max(0, Math.Min(255, red));
+                        newPixelData[offset + 1] = (byte)Math.Max(0, Math.Min(255, green));
+                        newPixelData[offset] = (byte)Math.Max(0, Math.Min(255, blue));
+                    }
+                }
+
+                BitmapSource processedBitmap = BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgr32, null, newPixelData, stride);
+                originalImage = ConvertBitmapSourceToBitmapImage(processedBitmap);
+                displayedImage.Source = processedBitmap;
+            }
+            else
+            {
+                MessageBox.Show("Najpierw wczytaj obraz.");
+            }
+        }
+
+        private void ApplyMaskFilter_Click(object sender, RoutedEventArgs e)
         {
 
         }

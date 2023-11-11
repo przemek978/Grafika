b2c5eb7a007e9078ad62c16374ee1c50799d2593
PKuczynski <przemek.ku2000@gmail.com>
2023-11-11 21:30:12 +0100

    bezier curve

--- Views/BezierCurve.xaml.cs
+++ Views/BezierCurve.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,123 +12,199 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Brushes = System.Windows.Media.Brushes;
+using Point = System.Windows.Point;
 
 namespace Grafika.Views
 {
     /// <summary>
     /// Logika interakcji dla klasy BezierCurve.xaml
     /// </summary>
-    public partial class BezierCurve: Window
+    public partial class BezierCurve : Window
     {
-        private int degree = 2; // Stopień krzywej Béziera
+        private int degree = 3; // Stopień krzywej Béziera
+        List<Point> controlPoints;
+        private Point selectedPoint;
+        private bool isDragging = false;
 
         public BezierCurve()
         {
             InitializeComponent();
+            controlPoints = new List<Point>();
+            DrawPoints();
         }
 
-
-
-        private void DrawBezierCurveButton_Click(object sender, RoutedEventArgs e)
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
         {
-            if (int.TryParse(degreeTextBox.Text, out int newDegree) && newDegree >= 1)
+            if (isDragging && e.LeftButton == MouseButtonState.Pressed)
+            {
+                int indexOfPoint = controlPoints.IndexOf(selectedPoint);
+                if (indexOfPoint == -1) return;
+
+                Point mousePosition = e.GetPosition(canvas);
+                selectedPoint.X = mousePosition.X;
+                selectedPoint.Y = mousePosition.Y;
+                controlPoints[indexOfPoint] = selectedPoint;
+                DrawBezierCurve();
+                DrawPoints();
+            }
+        }
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        private void SetPointClick(object sender, MouseButtonEventArgs e)
+        {
+
+            Point mousePosition = e.GetPosition(canvas);
+
+            bool existsPoint = controlPoints.Any(p => CalculateDistance(p, mousePosition) < 10);
+
+            if (int.TryParse(degreeTextBox.Text, out int newDegree))
             {
                 degree = newDegree;
-                DrawBezierCurve(degree);
             }
-            else
+
+            if (!existsPoint)
             {
-                MessageBox.Show("Invalid degree value. Please enter a positive integer.");
+                if (degree >= controlPoints.Count)
+                {
+                    controlPoints.Add(new Point(mousePosition.X, mousePosition.Y));
+                }
+                else
+                {
+                    MessageBox.Show($"Za dużo punktów, Stopień równy {degree} pozwala na dodanie {degree + 1} punktów");
+                }
+            }
+            else if (existsPoint)
+            {
+                isDragging = true;
+                selectedPoint = controlPoints.FirstOrDefault(p => CalculateDistance(p, mousePosition) < 10);
+            }
+            DrawBezierCurve();
+            DrawPoints();
+        }
+
+        public void DrawPoints()
+        {
+            foreach (var p in controlPoints)
+            {
+                Ellipse ellipse = new Ellipse
+                {
+                    Fill = Brushes.Red,
+                    Width = 8,
+                    Height = 8,
+                };
+                Canvas.SetLeft(ellipse, p.X - ellipse.Width / 2);
+                Canvas.SetTop(ellipse, p.Y - ellipse.Height / 2);
+                canvas.Children.Add(ellipse);
             }
         }
 
-        private void DrawBezierCurve(int degree)
+
+        private void DrawBezierCurve()
         {
             canvas.Children.Clear();
-
-            Point[] controlPoints = new Point[degree + 1];
-
-            for (int i = 0; i <= degree; i++)
-            {
-                double x = (canvas.ActualWidth / degree) * i;
-                double y = canvas.ActualHeight * (1 - Math.Pow(i / (double)degree, 2));
-                controlPoints[i] = new Point(x, y);
-            }
+            Path bezierPath = new Path();
+            bezierPath.Stroke = Brushes.Black;
+            bezierPath.StrokeThickness = 5;
 
             PathGeometry pathGeometry = new PathGeometry();
-            PathFigure pathFigure = new PathFigure
-            {
-                StartPoint = controlPoints[0]
-            };
+            PathFigure pathFigure = new PathFigure();
+            pathFigure.StartPoint = controlPoints[0];
+
+            PolyBezierSegment polyBezierSegment = new PolyBezierSegment();
+            polyBezierSegment.Points = new PointCollection(CalculateBezierPoints());
+
+            pathFigure.Segments.Add(polyBezierSegment);
             pathGeometry.Figures.Add(pathFigure);
-
-            for (int i = 1; i <= controlPoints.Length - degree; i += degree)
-            {
-                BezierSegment bezierSegment = new BezierSegment
-                {
-                    Point1 = controlPoints[i],
-                    Point2 = controlPoints[i + 1],
-                    Point3 = controlPoints[i + degree - 1]
-                };
-                pathFigure.Segments.Add(bezierSegment);
-            }
-
-            Path bezierPath = new Path
-            {
-                Data = pathGeometry,
-                Stroke = Brushes.Blue,
-                StrokeThickness = 2
-            };
+            bezierPath.Data = pathGeometry;
 
             canvas.Children.Add(bezierPath);
         }
 
-        private List<Point> GenerateControlPoints(int degree)
+        private PointCollection CalculateBezierPoints()
         {
-            List<Point> points = new List<Point>();
+            PointCollection bezierPoints = new PointCollection();
 
-            for (int i = 0; i <= degree; i++)
+            //for (int i = 0; i <= degree; i++)
+            //{
+            //    double t = (double)i / degree;
+            //    Point point = CalculateBezierPoint(t);
+            //    bezierPoints.Add(point);
+            //}
+
+            for (double t = 0; t <= 1; t += 0.005)
             {
-                double x = (canvas.ActualWidth / degree) * i;
-                double y = canvas.ActualHeight * (1 - Math.Pow(i / (double)degree, 2));
-                points.Add(new Point(x, y));
+                Point bezierPoint = CalculateBezierPoint(t);
+                bezierPoints.Add(bezierPoint);
             }
 
-            return points;
-        }
-
-        private List<Point> CalculateBezierPoints(List<Point> controlPoints)
-        {
-            List<Point> result = new List<Point>();
-            double step = 0.01;
-
-            for (double t = 0; t <= 1; t += step)
+            for (int i = 0; i < bezierPoints.Count - 1; i++)
             {
-                Point point = new Point(0, 0);
-                for (int i = 0; i <= degree; i++)
+                Line line = new Line
                 {
-                    double blend = BinomialCoefficient(degree, i) * Math.Pow(1 - t, degree - i) * Math.Pow(t, i);
-                    point.X += controlPoints[i].X * blend;
-                    point.Y += controlPoints[i].Y * blend;
-                }
-                result.Add(point);
+                    X1 = bezierPoints[i].X,
+                    Y1 = bezierPoints[i].Y,
+                    X2 = bezierPoints[i + 1].X,
+                    Y2 = bezierPoints[i + 1].Y,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 5
+                };
+
+                canvas.Children.Add(line);
+            }
+
+            return bezierPoints;
+        }
+
+        private Point CalculateBezierPoint(double t)
+        {
+            //int n = degree;
+            int n = controlPoints.Count - 1;
+
+            double x = 0;
+            double y = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                double berstein = Bernstein(n, i, t);
+                x += berstein * controlPoints[i].X;
+                y += berstein * controlPoints[i].Y;
+            }
+
+            return new Point(x, y);
+        }
+
+        private int Newton(int n, int i)  //https://pomax.github.io/bezierinfo/
+        {
+            int result = 1;
+
+            for (int j = 1; j <= i; j++)
+            {
+                result = result * (n - j + 1) / j;
             }
 
             return result;
         }
 
-        private double BinomialCoefficient(int n, int k)
+        private double Bernstein(int n, int i, double t)
         {
-            if (k < 0 || k > n)
-                return 0;
+            return Newton(n, i) * Math.Pow(1.0 - t, n - i) * Math.Pow(t, i);
+        }
 
-            double result = 1;
-            for (int i = 1; i <= k; i++)
-            {
-                result *= (n - i + 1);
-                result /= i;
-            }
-            return result;
+        private double CalculateDistance(Point point1, Point point2)
+        {
+            double deltaX = point2.X - point1.X;
+            double deltaY = point2.Y - point1.Y;
+            return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        }
+
+        private void ClearCanvas(object sender, RoutedEventArgs e)
+        {
+            canvas.Children.Clear();
+            controlPoints.Clear();
         }
 
     }
